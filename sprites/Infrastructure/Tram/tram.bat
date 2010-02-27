c4b94d30bd68b0ad4e32115ef319268faaf677e4
GeekToo <GeekToo@localhost>
2010-02-27 19:15:36 +0100

    Add:Tramtracks (Maquinista, Ben Robbins)

---
+++ sprites/Infrastructure/Tram/tram.bat
@@ -0,0 +1,47 @@
+pngcodec a 1254_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1255_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1256_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1257_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1258_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1259_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1260_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1261_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1262_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1263_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1264_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1269_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1270_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1271_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1272_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1277_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1278_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1279_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1280_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1281_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1282_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1283_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1284_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1285_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1286_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1287_z0.png x_offs=-124 y_offs=-6
+
+pngcodec r 1357_z0.png x_offs=-124 y_offs=-6
+pngcodec r 1358_z0.png x_offs=-124 y_offs=-6
+pngcodec a 1359_z0.png x_offs=-124 y_offs=-41
+pngcodec a 1360_z0.png x_offs=-124 y_offs=-9
+pngcodec a 1361_z0.png x_offs=-124 y_offs=-9
+pngcodec a 1362_z0.png x_offs=-124 y_offs=-41
+
+
+pngcodec a 1265_z0.png x_offs=-124 y_offs=-41
+pngcodec a 1288_z0.png x_offs=-124 y_offs=-41
+pngcodec a 1266_z0.png x_offs=-124 y_offs=-9
+pngcodec a 1289_z0.png x_offs=-124 y_offs=-9
+pngcodec a 1267_z0.png x_offs=-124 y_offs=-9
+pngcodec a 1290_z0.png x_offs=-124 y_offs=-9
+pngcodec a 1268_z0.png x_offs=-124 y_offs=-41
+pngcodec a 1291_z0.png x_offs=-124 y_offs=-41
+
+
+pngcodec a 1328_z0.png x_offs=-124 y_offs=+25
+pngcodec a 1329_z0.png x_offs=-125 y_offs=-23
